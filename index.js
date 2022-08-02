58d8654207fab140f1f77770336e770810c77766
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-08-02 12:25:45 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(){
+    return 5 + 6;
+}
+function subtract(){
+    return 10-5;
+}
+function multiply(){
+    return 4 * 6;
+}
+function divide (){
+    return 30 / 10;
+}
+function add(a, b) {
+return a + b;
+}
+add (310, 257);
+function subtract(a, b){
+    return a - b;
+}
+subtract (569, 569);
+function multiply(a, b){
+    return a * b;
+}
+multiply(24, 40);
+
+function divide(a, b){
+    return a / b;
+}
+divide(86, 56);
+
+function increment(n){
+    n++;
+    return n;
+}
+increment(151);
+
+function decrement(n){
+    n--;
+    return n;
+}
+decrement(46);
+
+function makeInt(n){
+    return n;
+
+}
+function preserveDecimal(n){
+    parseFloat("2.222");
+    return n;
+}
